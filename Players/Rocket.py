7f10adab80013bf833cd59ad268673e12d31d76d
Ofek Shachar <48984874+OfekSha@users.noreply.github.com>
2020-12-30 16:56:59 +0200

    base methods build for rocket and test with dealer

--- Players/Rocket.py
+++ Players/Rocket.py
@@ -7,32 +7,59 @@ import Operations.shamir as shamir
 import Player as P
 class Rocket(P.Player):
     shamirParts=[]
-    def __init__(self, Shamirsecret, p, q, g ,name,otpDic):
+    def __init__(self, p, q, g ,name,otpDic):
              # p, q, g - parms for signuture
-            self.Shamirsecret=Shamirsecret
             super().__init__(name, p, q, g,otpDic) 
             
-    def reciveShamirKeyPart(self,dealerShamirKeyPart):
-        if(not super().IdUser(name,dealerShamirKeyPart[1],dealerShamirKeyPart[2],dealerShamirKeyPart[3])):
+    def reciveShamirSecret(self,dealerName,secretEncrypted,dealerR,dealerS):
+        if(not super().IdUser(dealerName,secretEncrypted,dealerR,dealerS)):
             return False
-        #taking the shamir thingi and adding it to the shmir part list 
-        shamirPart=[]
-        temp=self.getShamirPart(ctext0,key)
-        shamirPart.append(temp)
-        shamirPart.append(self.getShamirPart(ctext1,key))
-        self.shamirParts.append(shamirPart)
+        self.__secret=super().decryptAsIntOTP(secretEncrypted,dealerName)
         return True
+    def reciveShare(self,personName,personR,personS,share):
+        # test if there are already 4 persons shares.
+        if (len(self.shamirParts))>=4:
+            return False
+        # share -> [0] not encrypted ; [1] encrypted
+        if(not super().IdUser(personName,share[1],personR,personS)):
+            return False
+        temp=super().decryptAsIntOTP(share[1],personName)
+        if temp is not None:
+            self.shamirParts.append(list(share[0],temp))
+            self._launch(self)
+    
+    def _launch(self):
+         if (len(self.shamirParts))<4: 
+             return False
+         temp=shamir.reconstructSecret(self.shamirParts)
+         if (temp is self.__secret):
+             self._launchEvent()
+             return True
+    def _launchEvent():
+        print("rocket launched!")
         
-        
-    def getShamirPart(self,ctext,key):
-         ptext=OTPBacedOnVigenereCipher.decrypt(ctext,key)
-         return Helper.wordToNum(ptext)
-         
-    def reconstructAndTestSecret(self):
-          #print(self.shamirParts)
-         return  shamir.reconstructSecret(self.shamirParts) ==self.Shamirsecret
-        
-        
+##-----------------------------TEST---------------------------------------
+if __name__ == '__main__':
+    import Dealer as D
+    N = 160
+    L = 1024
+    p, q, g = DSA.generate_params(L, N)
+    name, otpDic, otpDicRokcet="deal",{},{}
+    
+    otp=P.Player.createOTPBig()
+    otpDicRokcet["rocket"]=otp
+    dealer=D.Dealer(name, otpDic, otpDicRokcet, p, q, g)
+    otpDic[dealer.name]=otp
+    rocket= Rocket(p, q, g,"rocket",otpDic)
+    rocket.receiveSignaturePublicKey(dealer.y,dealer.name)
+    dealer.receiveSignaturePublicKey(rocket.y,rocket.name)
+    r,s=rocket.sign("givemeseacret")
+    ret= dealer.getSeacret(rocket.name,r,s)
+    #ret=[0-encrypted seacret][1-r][2-s]
+    rocket.reciveShamirSecret(dealer.name,ret[0],ret[1],ret[2])
     
     
-        
+   
+    
+    
+    
